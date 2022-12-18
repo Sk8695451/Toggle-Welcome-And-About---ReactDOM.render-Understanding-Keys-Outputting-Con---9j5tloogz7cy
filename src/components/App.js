b150e3df5b19317c12863674923010359ce00225
unknown <sk8695451>
2022-12-19 01:18:42 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [welcomepart, setWelcomepart] = useState(true);
+  const [aboutpart, setAboutpart] = useState(false);
+
+  const togglethis = () => {
+    setWelcomepart(!welcomepart);
+    setAboutpart(!aboutpart);
+  }
   return (
+
     <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      {
+        welcomepart ?
+          <div id="welcome-div">
+            <h1>Welcome to Newton School!!</h1>
+          </div> : null
+      }
+
+      {
+        aboutpart ?
+          <div id="about-div">
+
+
+            <h1>
+              Newton School is a neo-university providing highly immersive tech
+              learning to millions of students, to tap into new-age tech
+              opportunities.
+            </h1>
+          </div> : null
+      }
+      <button id="toggle" onClick={togglethis}>Toggle</button>
     </div>
   );
 };
